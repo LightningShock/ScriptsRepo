14de5e5e0a5f91e61a3b3ed5870b90e11d9acbfd
Youtubrer <youtubrer@user.cf>
2016-02-08 20:17:34 -0800

    v 0.10
    +Added Slave
    +Added Shorter Play function

---
+++ Scripts/MPP/LightningSS.meta.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         LightningShock's MPP script
+// @namespace    http://www.lightningshockscript.tk/
+// @version      0.10
+// @description  MPP ~Script
+// @author       LightningShock
+// @updateURL    http://www.lightningshockscript.tk/
+// @match        http://www.multiplayerpiano.com/*
+// @grant        none
+// @require      //www.multiplayerpiano.com/script.js
+// ==/UserScript==
