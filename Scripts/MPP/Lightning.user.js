d1a01ede4a6cc27d75cb4246d7e725b56931ad5c
Youtubrer <youtubrer@user.cf>
2016-02-08 20:21:40 -0800

    updated URL

--- Scripts/MPP/Lightning.user.js
+++ Scripts/MPP/Lightning.user.js
@@ -4,7 +4,8 @@
 // @version      0.10
 // @description  MPP ~Script
 // @author       LightningShock
-// @updateURL    http://www.lightningshockscript.tk/
+// @updateURL    https://raw.githubusercontent.com/Youtubrer/ScriptsRepo/master/Scripts/MPP/LightningSS.meta.js
+// @downloadURL  https://raw.githubusercontent.com/Youtubrer/ScriptsRepo/master/Scripts/MPP/Lightning.user.js
 // @match        http://www.multiplayerpiano.com/*
 // @grant        none
 // @require      //www.multiplayerpiano.com/script.js
